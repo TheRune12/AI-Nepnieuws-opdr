7dab2dcddf1afb0744ec4b3345d4d850f6001d3d
TheRune12 <rubenjhvanberkel@outlook.com>
2026-05-27 09:46:46 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<link rel="stylesheet" href="../assets/css/style.css">
+<img scr="#" alt="">
+<div class="header-bar">
+    <img src="#" alt="">
+    <div class="main-title">
+        AI & Nep Nieuws
+    </div>
+</div>
+<div class="nav-bar">
+    <div class="nav-buttons">
+        <div class="nav-button">
+            Home
+        </div>
+        <div class="nav-button">
+            Quiz
+        </div>
+        <div class="nav-button">
+            Tips
+        </div>
+    </div>
+</div>
